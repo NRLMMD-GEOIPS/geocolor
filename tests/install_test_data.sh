4eb5ceccf59b0fc472971065dff29dad9f5b86af
evrose54 <mrewrose@gmail.com>
2025-03-28 20:28:56 +0000

    added install script for geocolor data

---
+++ tests/install_test_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# # # Distribution Statement A. Approved for public release. Distribution unlimited.
+# # #
+# # # Author:
+# # # Naval Research Laboratory, Marine Meteorology Division
+# # #
+# # # This program is free software: you can redistribute it and/or modify it under
+# # # the terms of the NRLMMD License included with this program. This program is
+# # # distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+# # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
+# # # for more details. If you did not receive the license, for more information see:
+# # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
+
+
+if [[ ! -d $GEOIPS_PACKAGES_DIR/geoips ]]; then
+    echo "Must CLONE geoips repository into \$GEOIPS_PACKAGES_DIR location"
+    echo "to use test_all.sh testing utility."
+    echo ""
+    echo "export GEOIPS_PACKAGES_DIR=<path_to_geoips_cloned_packages>"
+    echo "git clone https://github.com/NRLMMD-GEOIPS/geoips $GEOIPS_PACKAGES_DIR/geoips"
+    echo ""
+    exit 1
+fi
+
+. $GEOIPS_PACKAGES_DIR/geoips/setup/check_system_requirements.sh test_data test_data_geocolor
